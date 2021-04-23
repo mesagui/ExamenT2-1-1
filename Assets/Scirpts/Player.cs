d366ee42e179b6b5a7c12c47a71ed770ffb4f2f8
Mike Estrada <mg.estrada.a@gmail.com>
2021-04-23 10:02:22 -0500

    first commit

---
+++ Assets/Scirpts/Player.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+
+{
+    public float velocidadCorrer = 10;
+    private Animator animator;
+    private Rigidbody2D rb;
+    public float fuerzaSalto = 10f;
+
+    private Transform tr;
+    
+
+    private SpriteRenderer sr;
+
+    private const int Animacion_Quieto = 0;
+    private const int Animacion_Correr = 2;
+    private const int Animacion_Saltar = 3;
+    private const int Animacion_Morir = 4;
+
+    private int EstadoSalto = 0;
+
+
+
+    public bool EstadoMuerte = false;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        tr = GetComponent<Transform>();
+    }
+
+
+    void Update()
+    {
+
+
+
+            if (EstadoMuerte == false)
+            {
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    MovimientoDerecha();
+                }
+                else if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    MovimientoIzquierda();
+                }
+                else if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    Saltar();
+                }
+                
+
+                else
+                {
+                    EstarQuieto();
+                }
+            }
+            else
+            {
+                CambiarAnimacion(Animacion_Morir);
+                Debug.Log("Muerte");
+            }
+        
+        
+    }
+
+    private void CambiarAnimacion(int animation)
+    {
+        animator.SetInteger("estado", animation);
+    }
+
+    private void Saltar()
+    {
+        rb.velocity = Vector2.up * fuerzaSalto;
+        CambiarAnimacion(Animacion_Saltar);
+
+    }
+
+    private void MovimientoIzquierda()
+    {
+        tr.localScale = new Vector3(-1.3f,1.5f,1);
+        rb.velocity = new Vector2(-velocidadCorrer, rb.velocity.y);
+        CambiarAnimacion(Animacion_Correr);
+        if (Input.GetKey(KeyCode.C))
+        {
+            CambiarAnimacion(Animacion_Correr);
+            tr.localScale = new Vector3(-1.3f,1.5f,1);
+            rb.velocity = new Vector2(-velocidadCorrer, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Saltar();
+        }
+    }
+
+    private void MovimientoDerecha()
+    {
+        tr.localScale = new Vector3(1.3f,1.5f,1);
+        rb.velocity = new Vector2(velocidadCorrer, rb.velocity.y);
+        CambiarAnimacion(Animacion_Correr);
+        if (Input.GetKey(KeyCode.C))
+        {
+            CambiarAnimacion(Animacion_Correr);
+            tr.localScale = new Vector3(1.3f,1.5f,1);
+            rb.velocity = new Vector2(velocidadCorrer, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Saltar();
+        }
+    }
+
+    private void EstarQuieto()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        CambiarAnimacion(Animacion_Quieto);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Muerte")
+        {
+            EstadoMuerte = true;
+        }
+
+        if (collision.gameObject.tag == "Suelo")
+        {
+            EstadoSalto = 0;
+            Debug.Log("aaaaaa");
+        }
+
+    }
+    
+}
---
+++ Assets/Scirpts/Zombie.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Vector3 = System.Numerics.Vector3;
+
+public class Zombie : MonoBehaviour
+{
+    public float velocidad = 8f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    
+
+    private bool PosicionA = false;
+
+    private const int Animacion_Caminar = 0;
+    private const int Animacion_Morir = 1;
+    
+    private Animator animator;
+    private BoxCollider2D bx;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        bx = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (PosicionA == false)
+            {
+                sr.flipX = false;
+                rb.velocity = new Vector2(velocidad, rb.velocity.y);
+            }
+            else
+            {
+                sr.flipX = true;
+                rb.velocity = new Vector2(-velocidad, rb.velocity.y);
+            }
+        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Posicion2")
+        {
+            PosicionA = true;
+        }
+
+        if (collision.gameObject.tag == "Posicion1")
+        {
+            PosicionA = false;
+        }
+
+
+    }
+    private void CambiarAnimacion(int animation)
+    {
+        animator.SetInteger("estado", animation);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "chocazombie")
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+
+}
